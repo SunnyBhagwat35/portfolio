689fc0789275ee7a29a098f174a475b50600e00b
Sunny Bhagwat <sunnybhagwat35@gmail.com>
2026-08-10 12:57:09 +0000

    base adede

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/"
   // If deploying to GitHub Pages under a repo subpath, set base: "/<repo-name>/"
   // For Vercel, leave base as default ("/").
 });
